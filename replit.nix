185b5c30360d81f558da459274c8a00fd5fed216
Rémy B <KirrimK@users.noreply.github.com>
2022-10-28 12:46:22 +0000

    added more packages to replit env (used for testing and displaying version number)

--- replit.nix
+++ replit.nix
@@ -3,5 +3,9 @@
         pkgs.ocaml
         pkgs.dune_2
         pkgs.ocamlPackages.menhir
+        pkgs.ocamlPackages.ppxlib
+        pkgs.ocamlPackages.bisect_ppx
+        pkgs.ocamlPackages.findlib
+        pkgs.ocamlPackages.dune-build-info
     ];
 }
